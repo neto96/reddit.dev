d217d623c4601886cf75432181e586124046ba10
Ernesto <neto_neto@hotmail.com>
2016-08-18 15:03:03 -0500

    Add date/time on posts with Chicago timezone

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <table class="table table-condensed">
     <tr>
-        <th><h1>Title</h1></th>
-        <th><h1>URL</h1></th>
-        <th><h1>Description</h1></th>
-        <th><h3>Submitted by:</h3></th>
+        <th><h3>Title</h3></th>
+        <th><h3>URL</h3></th>
+        <th><h3>Description</h3></th>
+        <th><h3>Created:</h3></th>
     </tr>
     @foreach($posts as $post)
         <tr>
             <td>{{ $post->title }}</td>
             <td><a href="{{$post->url}}">{{ $post->url }}</a></td>
             <td>{{ $post->content }}</td>
-            <td>{{ $post->created_by }}</td>
+            <td><strong>By:</strong>{{ $post->created_by }}<br><strong>On:</strong> {{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}</td>
         </tr>
     @endforeach
 </table>
